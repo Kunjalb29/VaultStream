441fcd414819ef7f709ddc398ea322a4d02f152c
Microsoft Engineer <engineer@microsoft.com>
2026-03-16 17:07:17 +0530

    feat(api): Integrate Serilog for structured semantic logging

--- VaultStream.Api/Program.cs
+++ VaultStream.Api/Program.cs
@@ -2,9 +2,19 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using VaultStream.Infrastructure.Data;
+using Serilog;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure Serilog
+builder.Host.UseSerilog((context, loggerConfiguration) =>
+{
+    loggerConfiguration
+        .ReadFrom.Configuration(context.Configuration)
+        .Enrich.FromLogContext()
+        .WriteTo.Console();
+});
 
 // Add services to the container.
 builder.Services.AddControllers();
@@ -20,6 +30,8 @@ var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.UseMiddleware<VaultStream.Api.Middleware.GlobalExceptionMiddleware>();
+
+app.UseSerilogRequestLogging();
 
 app.UseSwagger();
 app.UseSwaggerUI();
