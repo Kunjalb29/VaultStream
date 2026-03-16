45bca91d4402f39b28dc0c3e512a5f2f6f8373d3
Microsoft Engineer <engineer@microsoft.com>
2026-03-16 17:03:36 +0530

    feat(data): Implement generic Repository pattern

---
+++ VaultStream.Application/Interfaces/IRepository.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace VaultStream.Application.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<T?> GetByIdAsync(Guid id);
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<IEnumerable<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity);
+        Task AddRangeAsync(IEnumerable<T> entities);
+        void Remove(T entity);
+        void RemoveRange(IEnumerable<T> entities);
+    }
+}
