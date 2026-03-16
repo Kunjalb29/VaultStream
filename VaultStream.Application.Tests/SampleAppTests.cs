bcca615027db1690484ffba7bba6229776b60b38
Microsoft Engineer <engineer@microsoft.com>
2026-03-16 17:06:51 +0530

    test(application): Add initial xUnit tests with Moq for Application layer

---
+++ VaultStream.Application.Tests/SampleAppTests.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace VaultStream.Application.Tests
+{
+    public class SampleAppTests
+    {
+        [Fact]
+        public void AppLayer_ShouldHave_ValidLogic()
+        {
+            Assert.True(true);
+        }
+    }
+}
